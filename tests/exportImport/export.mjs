02aa17e5584f4ab157a6941b316f5bae951e9272
Om Gutty <om.gutty@gmail.com>
2026-03-08 14:58:35 +0530

    importexport1

---
+++ tests/exportImport/export.mjs
@@ -0,0 +1,12 @@
+
+
+export let name= "om";
+export let age= 35;
+
+export function add (a,b){
+    console.log(a+b);
+    return a+b;
+}
+
+
+
---
+++ tests/exportImport/import.mjs
@@ -0,0 +1,9 @@
+
+
+import {name, age, add} from './export.mjs';
+
+
+console.log(name);
+console.log(age);
+let addtion =add(5,5);
+console.log(addtion);
