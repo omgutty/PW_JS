f38c099d8744b5c4bf00d6c04ec7160881433907
Om Gutty <om.gutty@gmail.com>
2026-03-10 14:53:21 +0530

    class, methods, object creation ,

--- tests/class/human.js
+++ tests/class/human.js
@@ -14,14 +14,46 @@ class Human {
 
 let objmohan= new Human("mohan",25);
 let objom= new Human("om",30);
-
+//calling variables inside the class
 console.log(objmohan.age);
 console.log(objmohan.name);
 console.log(objom.name);
 console.log(objom.age);
 
+
+
+
+
 class animal{
+    constructor(name){
+        this.animalname=name;
+    }
+
+    legs(int){
+        console.log(" number of legs : "+ int);
+        console.log(`${this.animalname} animal have total legs of ${int}`);
+    }
+
+    dogsound(){
+        return console.log("Barking ");
+    }
+
+    running = function(string){
+        console.log(`${string} can run faster`);
+    }
+
+    jumping = ()=>{
+        console.log("jumping arrow");
+    }
 
 }
 
-let dog= new animal();
+//calling the methods inside the class
+let dogobj= new animal("dog");
+dogobj.legs(4);
+dogobj.dogsound();
+dogobj.running("dog");
+dogobj.jumping();
+
+//let rabitobj= new animal("Rabit");
+//rabitobj.legs(2);
